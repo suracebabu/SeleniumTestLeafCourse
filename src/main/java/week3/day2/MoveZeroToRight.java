51fa55aee796ed62c50e7e84c254936710f145a0
surac <surac@192.168.1.22>
2021-05-23 23:00:29 -0400

    may 23

--- src/main/java/week3/day2/MoveZeroToRight.java
+++ src/main/java/week3/day2/MoveZeroToRight.java
@@ -7,59 +7,57 @@ public class MoveZeroToRight {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		int[] num = {4,0,3,0,1,5,2,0};
+		int[] numArray = {4,0,3,0,1,5,2,0};
 		
 		int zeroCount=0;
 		int swapNum;
 		
-		
-		List<Integer> values = new ArrayList<Integer>();
-		
-		for (Integer integer : num) {
-			if(integer!=0)
+		for (int i = 0; i <(numArray.length-2); i++) {
+			//check if first number is zero, if zero -> shift right and swap the right number to left
+			if(numArray[i]==0)
+			{
+				
+				if(numArray[i+1]==0)
 				{
-				values.add(integer);
-				zeroCount++;
+					
+					numArray[i]=numArray[i+2];
+					numArray[i+2]=0;
+					
+				}
+				else {
+					numArray[i]= numArray[i+1];
+					numArray[i+1]=0;
 				}
 			}
-	
-		
-		for(int i=0;i<(num.length-zeroCount);i++)
-		{
-		values.add(0);
-		}
-		System.out.println(zeroCount);
-		System.out.println(values);
-		
-		
-//	
-		for (Integer integer : num) {
 			
-			System.out.println(integer);
+			else {
+				continue;
+			}
 		}
 		
+		
+		
+//		List<Integer> values = new ArrayList<Integer>();
+//		
+//		for (Integer integer : num) {
+//			if(integer!=0)
+//				{
+//				values.add(integer);
+//				zeroCount++;
+//				}
+//			}
+//	
+//		
+//		for(int i=0;i<(num.length-zeroCount);i++)
+//		{
+//		values.add(0);
+//		}
+//		System.out.println(zeroCount);
+//		System.out.println(values);
+
+	for (Integer integer : numArray) {
+		
+		System.out.println(integer);
 	}
 	
-	
-//	for (int i = 0; i <(num.length-2); i++) {
-//		//check if first number is zero, if zero -> shift right and swap the right number to left
-//		if(num[i]==0)
-//		{
-//			
-//			if(num[i+1]==0)
-//			{
-//				num[i]=num[i+2];
-//				num[i+2]=0;
-//				
-//			}
-//			else {
-//			num[i]= num[i+1];
-//			num[i+1]=0;
-//			}
-//		}
-//		
-//		else {
-//			continue;
-//		}
-//	}
-}
+	}}
